b35a99da5f5a2da05d295cfc26f4d2c0b93d0eaf
ChandanaThoti <chandana.thoti@everest.engineering>
2026-01-02 14:27:54 +0530

    Add task service with addDbTask

---
+++ src/services/taskService/taskService.ts
@@ -0,0 +1,12 @@
+import { db } from "../../config/firebaseConfig";
+import { Task } from "../../types/Task";
+
+const taskCollection = db.collection("tasks");
+
+export const addDbTask = (task: Task) => {
+  if (!task) {
+    return false;
+  }
+  taskCollection.doc().set(task);
+  return true;
+};
