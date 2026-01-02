356a007ca2edf49675e5e664f241abb9d4d6fa27
ChandanaThoti <chandana.thoti@everest.engineering>
2026-01-02 14:28:17 +0530

    Add task controller with addTask service

---
+++ src/controller/taskController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { addDbTask } from "../services/taskService/taskService";
+
+export const addTask = (req: Request, res: Response) => {
+  try {
+    const { taskName, description, status, priority, deadline } = req.body;
+    if (!taskName || !description || !status || !priority || !deadline) {
+      return res.status(400).send("Please fill all fields");
+    }
+    const task = addDbTask({
+      taskName,
+      description,
+      status,
+      priority,
+      deadline,
+    });
+    res.status(201).send(task);
+  } catch {
+    res.status(500).send("Internal Server Error");
+  }
+};
