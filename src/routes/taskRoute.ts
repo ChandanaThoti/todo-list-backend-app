b59345d265f7ee25583e27f5be575cfce5934378
ChandanaThoti <chandana.thoti@everest.engineering>
2026-01-02 18:33:25 +0530

    Add get http method to viewTasks

--- src/routes/taskRoute.ts
+++ src/routes/taskRoute.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { addTask } from "../controller/taskController/taskController";
+import {
+  addTask,
+  viewTasks,
+} from "../controller/taskController/taskController";
 
 const taskRoute = express.Router();
 
 taskRoute.post("/tasks", addTask);
+taskRoute.get("/tasks", viewTasks);
 
 export default taskRoute;
