95cc7d59be6fd83c5a23df28c58c68bb545859ce
ChandanaThoti <chandana.thoti@everest.engineering>
2026-01-02 17:31:34 +0530

    Add viewTasks service to view tasks

--- src/services/taskService/taskService.ts
+++ src/services/taskService/taskService.ts
@@ -11,3 +11,9 @@ export const addDbTask = async (task: Task): Promise<boolean> => {
   await taskCollection.doc().set(task);
   return true;
 };
+
+export const viewDbTasks = async () => {
+  const snapShot = await taskCollection.get();
+  const tasks = snapShot.docs.map((task) => task.data());
+  return tasks;
+};
